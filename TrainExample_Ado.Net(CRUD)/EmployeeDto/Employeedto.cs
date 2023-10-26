27dbab4e26686f99f383b84c2e1fd4f4a34b35ba
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-10-26 21:04:58 +0500

    Dto Class created

---
+++ TrainExample_Ado.Net(CRUD)/EmployeeDto/Employeedto.cs
@@ -0,0 +1,14 @@
+﻿using TrainExample_Ado.Net_CRUD_.Enum;
+
+namespace TrainExample_Ado.Net_CRUD_.EmployeeDto
+{
+    public class Employeedto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+}
